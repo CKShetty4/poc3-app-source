81c9444bbbf8ad231903b3fc0163e6129c31fed1
Chinmay K Shetty <ckshetty004@gmail.com>
2026-07-01 15:56:22 +0530

    Checking Pipeline2
    Updated the response message for the root endpoint.

--- src/main/java/com/poc3/App.java
+++ src/main/java/com/poc3/App.java
@@ -4,9 +4,8 @@ import io.javalin.Javalin;
 
 public class App {
     public static void main(String[] args) {
-        // Starts an absolute basic microservice running on port 8080
         var app = Javalin.create().start(8080);
         
-        app.get("/", ctx -> ctx.result("Hello from PoC 3! GitOps Pipeline Working Perfectly."));
+        app.get("/", ctx -> ctx.result("Hello from PoC 3! GitOps Pipeline Working Perfectly. and Ready"));
     }
 }
