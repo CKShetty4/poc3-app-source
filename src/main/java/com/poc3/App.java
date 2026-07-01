b825e6d16aeaf9c31f256025712a2d4491de6ec3
Chinmay K Shetty <ckshetty004@gmail.com>
2026-07-01 16:42:40 +0530

    Checking Pipeline4

--- src/main/java/com/poc3/App.java
+++ src/main/java/com/poc3/App.java
@@ -6,6 +6,6 @@ public class App {
     public static void main(String[] args) {
         var app = Javalin.create().start(8080);
         
-        app.get("/", ctx -> ctx.result("Hello from PoC 3! GitOps Pipeline Working Perfectly and Ready to be tested."));
+        app.get("/", ctx -> ctx.result("Hello Team, This is Demo."));
     }
 }
